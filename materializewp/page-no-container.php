baef3956eb09f8c502ff26ca72ead6485444eea3
Akshay Dhiman <34573958+theakshaydhiman@users.noreply.github.com>
2018-04-22 21:37:22 +0530

    0.1.1
    edit page-no-container.php
    add theme screenshot.png

--- materializewp/page-no-container.php
+++ materializewp/page-no-container.php
@@ -9,7 +9,7 @@
 <?php get_header(); ?>
   <main role="main">
     <div class="row">
-      <div class="col s12 m12 l8 blog-main">
+      <div class="col s12 m12 l12 blog-main">
         <?php if(have_posts()) : ?>
           <?php while(have_posts()) : the_post(); ?>
             <?php get_template_part('content', get_post_format()); ?>
@@ -17,8 +17,7 @@
         <?php else : ?>
           <p><?php __('No Pages Found') ?></p>
         <?php endif; ?>
-      </div><!-- /.blog-main -->
-      <?php get_sidebar(); ?>
-    </div><!-- /.row -->
-  </main><!-- /.container -->
+      </div>
+    </div>
+  </main>
 <?php get_footer(); ?>
