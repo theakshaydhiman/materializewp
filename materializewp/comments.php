fa744afd3584c5672d2ef192064f7331703504e2
theakshaydhiman <akshay.virgo@gmail.com>
2018-04-28 21:39:16 +0530

    0.1.5

--- materializewp/comments.php
+++ materializewp/comments.php
@@ -1,70 +1,70 @@
 <div class="comments">
 
-<?php if (have_comments()) : ?>
+  <?php if (have_comments()) : ?>
 
-	<h3>Comments</h3>
+    <h3>Comments</h3>
 
-<?php endif; ?>
-<div class="comment-list">
-<?php
+  <?php endif; ?>
+  <div class="comment-list">
+    <?php
 
-$args = array(
-	'walker'            => null,
-	'max_depth'         => '',
-	'style'             => 'div',
-	'callback'          => null,
-	'end-callback'      => null,
-	'type'              => 'all',
-	'reply_text'        => 'Reply',
-	'page'              => '',
-	'per_page'          => '',
-	'avatar_size'       => 120,
-	'reverse_top_level' => null,
-	'reverse_children'  => '',
-	'format'            => 'html5', // or 'xhtml' if no 'HTML5' theme support
-	'short_ping'        => false,   // @since 3.6
-  'echo'              => true,     // boolean, default is true
-	);
+    $args = array(
+      'walker'            => null,
+      'max_depth'         => '',
+      'style'             => 'div',
+      'callback'          => null,
+      'end-callback'      => null,
+      'type'              => 'all',
+      'reply_text'        => 'Reply',
+      'page'              => '',
+      'per_page'          => '',
+      'avatar_size'       => 120,
+      'reverse_top_level' => null,
+      'reverse_children'  => '',
+      'format'            => 'html5', // or 'xhtml' if no 'HTML5' theme support
+      'short_ping'        => false,   // @since 3.6
+      'echo'              => true,     // boolean, default is true
+      );
 
-wp_list_comments($args, $comments);
-?>
-</div>
-<?php
-$commenter = wp_get_current_commenter();
-$req = get_option( 'require_name_email' );
-$aria_req = ( $req ? " aria-required='true'" : '' );
-$fields =  array(
+    wp_list_comments($args, $comments);
+    ?>
+  </div>
+  <?php
+  $commenter = wp_get_current_commenter();
+  $req = get_option( 'require_name_email' );
+  $aria_req = ( $req ? " aria-required='true'" : '' );
+  $fields =  array(
 
-  'author' =>
-    '<p class="comment-form-author input-field"><label for="author">' . __( 'Name', 'domainreference' ) .
-    ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
-    '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
-    '" size="30"' . $aria_req . ' /></p>',
+    'author' =>
+      '<p class="comment-form-author input-field"><label for="author">' . __( 'Name', 'domainreference' ) .
+      ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+      '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
 
-  'email' =>
-    '<p class="comment-form-email input-field"><label for="email">' . __( 'Email', 'domainreference' ) .
-    ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
-    '<input id="email" name="email" class="validate" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
-    '" size="30"' . $aria_req . ' /></p>',
+    'email' =>
+      '<p class="comment-form-email input-field"><label for="email">' . __( 'Email', 'domainreference' ) .
+      ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
+      '<input id="email" name="email" class="validate" type="email" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+      '" size="30"' . $aria_req . ' /></p>',
 
-  'url' =>
-    '<p class="comment-form-url input-field"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
-    '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
-    '" size="30" /></p>',
-);
+    'url' =>
+      '<p class="comment-form-url input-field"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+      '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+      '" size="30" /></p>',
+  );
 
 
-$comments_args = array(
-        'label_submit'=>'Submit',
-        'class_submit'=>'btn',
-        'title_reply'=>'Leave a Comment',
-        'comment_notes_after' => '',
-        'comment_field' => '<p class="comment-form-comment input-field"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" aria-required="true" class="materialize-textarea"></textarea></p>',
-        'fields' => apply_filters( 'comment_form_default_fields', $fields ),
-        'submit_button' => '<button name="%1$s" type="submit" id="%2$s" class="%3$s btn waves-effect waves-light">%4$s</button>'
-);
+  $comments_args = array(
+          'label_submit'=>'Submit',
+          'class_submit'=>'btn',
+          'title_reply'=>'Leave a Comment',
+          'comment_notes_after' => '',
+          'comment_field' => '<p class="comment-form-comment input-field"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><textarea id="comment" name="comment" aria-required="true" class="materialize-textarea"></textarea></p>',
+          'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+          'submit_button' => '<button name="%1$s" type="submit" id="%2$s" class="%3$s btn waves-effect waves-light">%4$s</button>'
+  );
 
-comment_form($comments_args);
+  comment_form($comments_args);
 
-?>
+  ?>
 </div>
--- materializewp/post-templates/content.php
+++ materializewp/post-templates/content.php
@@ -1,33 +1,52 @@
-<div class="blog-post">
+<?php if(is_single() || is_page()): ?>
 
-  <h1 class="blog-post-title">
-    <?php if(is_single() || is_page()): ?>
-      <?php the_title(); ?>
-    <?php else : ?>  
-      <a href="<?php the_permalink(); ?>">
-      <?php the_title(); ?>
-    <?php endif; ?>
-    </a>
-  </h1>
+  <div class="blog-post">
 
-  <p class="blog-post-meta grey-text text-darken-1">Written by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | <?php the_time('F j, Y'); ?> | <a href="<?php comments_link(); ?>"><?php comments_number('No Comments', '1 Comment', '% Comments'); ?></a>
-  </p>
+    <div class="card-panel">
+      <h1 class="blog-post-title"><?php the_title(); ?></h1>
+      <p class="blog-post-meta grey-text text-darken-1">Written by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | <?php the_time('F j, Y'); ?> | <a href="<?php comments_link(); ?>"><?php comments_number('No Comments', '1 Comment', '% Comments'); ?></a>
+      </p>
+      <div class="post-thumb">
+        <?php if(has_post_thumbnail()) : ?>
+          <?php the_post_thumbnail('large', array( 'class' => 'post-thumb' )); ?>
+        <?php endif; ?>
+      </div>
+      <?php the_content(); ?>
+    </div>
+    
+    <!-- Display comments -->
+    <?php if( is_single() ):
+      if( comments_open() ) { 
+        comments_template();
+      } else { 
+        echo '<p>Comments are closed for this post.</p>';
+        }
+      endif;
+    ?>
 
-  <div class="post-thumb">
-    <?php if(has_post_thumbnail()) : ?>
-      <?php the_post_thumbnail('large', array( 'class' => 'post-thumb' )); ?>
-    <?php endif; ?>
   </div>
 
-  <?php if(is_single() || is_page()): ?>
-      <?php the_content(); ?>
-    <?php else : ?>  
-      <?php the_excerpt(); ?>
-    <?php endif; ?>
-    
-  <?php if(is_single()): ?>
-    <?php if( comments_open() ) { comments_template(); } else { echo '<p>Comments are closed for this post.</p>'; } ?>
-  <?php endif; ?>
+<?php else : ?>
 
+  <div class="blog-post-excerpt">
+    <a href="<?php the_permalink(); ?>">
 
-</div>
+      <div class="card hoverable">
+
+        <div>
+          <?php if(has_post_thumbnail()) : ?>
+            <?php the_post_thumbnail('medium_large', array('class' => 'responsive-img') ); ?>
+          <?php endif; ?>
+        </div>
+
+        <div class="card-content">
+          <h1 class="blog-post-title"><?php the_title(); ?></h1>
+          <p class="blog-post-meta grey-text text-darken-1">Written by <?php the_author(); ?> | <?php the_time('F j, Y'); ?> | <?php comments_number('No Comments', '1 Comment', '% Comments'); ?></p>
+          <br>
+          <?php the_excerpt(); ?>
+        </div>
+
+      </div>
+    </a>
+  </div>
+<?php endif; ?>
