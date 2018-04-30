0c9e97f1a87e9cc07ed683ad5b759fe564ab0ed9
theakshaydhiman <akshay.virgo@gmail.com>
2018-04-30 14:45:27 +0530

    0.1.10

--- materializewp/inc/functions/theme-support.php
+++ materializewp/inc/functions/theme-support.php
@@ -3,10 +3,10 @@
 // Theme Support
 
 function materializewp_setup(){
-    add_theme_support('menus');
-    register_nav_menu('primary', "Primary Header Navigation");
-    register_nav_menu('secondary', "Footer Navigation");
-    register_nav_menu('social', "Social Links");
+	add_theme_support('menus');
+	register_nav_menu('primary', "Primary Header Navigation");
+	register_nav_menu('secondary', "Footer Navigation");
+	register_nav_menu('social', "Social Links");
 }
 add_action('init','materializewp_setup');
 
@@ -14,19 +14,19 @@ add_theme_support('custom-background');
 add_theme_support('post-thumbnails');
 
 add_theme_support('post-formats', array(
-    'aside',
-    'gallery',
-    'link',
-    'image',
-    'quote',
-    'video',
-    'audio'
+	'aside',
+	'gallery',
+	'link',
+	'image',
+	'quote',
+	'video',
+	'audio'
 ));
 
 add_theme_support('html5', array(
-    'comment-list',
-    'comment-form',
-    'search-form'
+	'comment-list',
+	'comment-form',
+	'search-form'
 ));
 
 // Let WordPress handle the <title> tag.
@@ -34,13 +34,13 @@ add_theme_support( 'title-tag' );
 
 // Excerpt length control
 function set_excerpt_length(){
-    return 45;
+	return 45;
 }
 add_filter('excerpt_length', 'set_excerpt_length');
 
 // Head function, remove wp version
 function materializewp_remove_version() {
-    return '';
+	return '';
 }
 add_filter('the_generator','materializewp_remove_version');
 
@@ -51,4 +51,11 @@ function add_image_class($class){
 }
 add_filter('get_image_tag_class','add_image_class');
 
+// Add class 'video-container' to all post iframe videos
+function iframe_video( $html ) {
+	$html = str_replace('<iframe', '<div class="video-container"><iframe', $html);
+	$html = str_replace('</iframe>', '</iframe></div>', $html);
+	return $html;
+}
+add_filter('embed_oembed_html', 'iframe_video', 99, 4);
 ?>
--- materializewp/post-templates/content-link.php
+++ materializewp/post-templates/content-link.php
@@ -1,4 +1,4 @@
-<div class="blog-post post-aside">
+<div class="blog-post">
 	<div class="card-panel grey lighten-4">
 		<div class="card-content">
 			<small><?php the_author(); ?> on <?php echo get_the_date(); ?></small>
