2f878aaec66129a4c049c5055d98966d3a745d5a
theakshaydhiman <akshay.virgo@gmail.com>
2018-05-20 17:54:53 +0530

    removed custom.css, importing entire materialize css to style.scss

--- materializewp/functions.php
+++ materializewp/functions.php
@@ -5,10 +5,9 @@
 
 // Enqueue Scripts and Stylesheets
 function materializewp_scripts() {
-	wp_enqueue_style( 'materialize', get_theme_file_uri() . '/assets/css/materialize.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'custom_css', get_theme_file_uri() . '/assets/css/custom.css', array(), '1.0.0', 'all' );
-	wp_enqueue_style( 'materialize-icons', 'https://fonts.googleapis.com/icon?family=Material+Icons', '1.0.0', 'all' );
+	wp_enqueue_style( 'material_icons', 'https://fonts.googleapis.com/icon?family=Material+Icons', '1.0.0', 'all' );
 	wp_enqueue_style( 'roboto', 'https://fonts.googleapis.com/css?family=Roboto', '1.0.0', 'all' );
+	wp_enqueue_style( 'style', get_theme_file_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
 	wp_enqueue_script( 'materialize', get_theme_file_uri() . '/assets/js/materialize.min.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'materializewp_scripts' );
