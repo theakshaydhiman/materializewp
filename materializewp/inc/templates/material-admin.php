3a3b10d73e4ba736aaf464446e446d05374d11ed
theakshaydhiman <akshay.virgo@gmail.com>
2018-05-31 09:48:58 +0530

    updated admin page

--- materializewp/inc/templates/material-admin.php
+++ materializewp/inc/templates/material-admin.php
@@ -1,2 +1,16 @@
-<h1>MaterializeWP Theme Options</h1>
-<p>Welcome to the theme options</p>
+<h1>MaterializeWP <small>v<?php
+$mwp_theme = wp_get_theme();
+echo $mwp_theme->get( 'Version' );
+?></small></h1>
+
+<hr>
+
+<h2>Theme Updates</h2>
+<p>The latest version of the MaterializeWP theme can be downloaded from <a href="https://github.com/theakshaydhiman/materializewp/releases" target="_blank" rel="noopener">here</a>.</p>
+
+<h2>Contributions</h2>
+<p>Feel free to <a href="https://github.com/theakshaydhiman/materializewp/issues" target="_blank" rel="noopener">report issues</a> or <a href="https://github.com/theakshaydhiman/materializewp/pulls" target="_blank" rel="noopener">request new features</a> on GitHub. Your contributions are really appreciated.</p>
+
+<br><br>
+
+<p><em>Powered by <a href="https://github.com/theakshaydhiman/materializewp" target="_blank" rel="noopener">MaterializeWP</a>, <a href="https://github.com/theakshaydhiman/materializewp/blob/master/LICENSE" target="_blank" rel="noopener">MIT License</a>.</em></p>
