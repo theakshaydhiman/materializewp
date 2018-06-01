44579e1b115f972de2567ea6cd32ad357db494ac
theakshaydhiman <akshay.virgo@gmail.com>
2018-06-01 12:40:12 +0530

    updated card structure in post author partial

--- materializewp/inc/partials/partials-post-author.php
+++ materializewp/inc/partials/partials-post-author.php
@@ -1,14 +1,18 @@
-<div class="card-panel">
-  <div class="row">
-    <div class="col s12 m4 l3 center-align">
-      <?php echo get_avatar( get_the_author_meta( 'ID' ), 125 ); ?>
+<div class="card">
+  <div class="card-content">
+    <div class="row">
+      <div class="col s12 m4 l3 center-align">
+        <?php echo get_avatar( get_the_author_meta( 'ID' ), 125 ); ?>
+      </div>
+      <div class="col s12 m8 l9">
+        <h4 class="h5"><?php echo get_the_author_meta( 'display_name' ); ?></h4>
+        <p class="grey-text text-darken-2"><?php echo get_the_author_meta( 'description' ); ?></p>
+      </div>
     </div>
-    <div class="col s12 m8 l9">
-      <h4 class="h5"><?php echo get_the_author_meta( 'display_name' ); ?></h4>
-      <p class="grey-text text-darken-2"><?php echo get_the_author_meta( 'description' ); ?></p>
-      <?php if( get_the_author_meta( 'url' ) ): ?>
-        <a href="<?php echo get_the_author_meta( 'url' ); ?>" target="_blank" rel="noopener" title="Visit <?php echo get_the_author_meta( 'first_name' ); ?>'s website."><i class="material-icons">public</i></a>
-      <?php endif; ?>
-    </div>
+  </div>
+  <div class="card-action">
+    <?php if( get_the_author_meta( 'url' ) ): ?>
+      <a href="<?php echo get_the_author_meta( 'url' ); ?>" class="waves-effect red-text text-darken-1" target="_blank" rel="noopener"><i class="material-icons left">public</i> Visit <?php echo get_the_author_meta( 'first_name' ); ?>'s website</a>
+    <?php endif; ?>
   </div>
 </div>
